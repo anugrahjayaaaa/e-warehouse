d3afec6b70d7c9df66e456cf8c30e36324127d94
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-07-06 14:38:40 +0700

    setup liveware structure for super admin

---
+++ app/Livewire/Superadmin/Stock/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Superadmin\Stock;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.superadmin.stock.index');
+    }
+}
